fad32f8408a196c71dcfe50b02b1e8a4b1f3e6cb
Rodolfo Delduca <usodelduca@gmail.com>
2023-11-09 16:57:17 +1300

    creates logic to get People data from API

--- client/components/People.tsx
+++ client/components/People.tsx
@@ -1,3 +1,40 @@
+import { useEffect, useState } from 'react'
+import request from 'superagent'
+
 export function People() {
-  return <p>This is the People of SW</p>
+  const [people, setPeople] = useState([])
+
+  async function getPeople() {
+    const res = await request.get('https://swapi.dev/api/people/')
+    setPeople(res.body)
+  }
+  useEffect(() => {
+    getPeople()
+  }, [])
+
+  // const names = people?.results
+  console.log('NAAAAAAMES', people)
+  if (people) {
+    const dataKeys = Object.keys(people)
+    return (
+      <>
+        <p>This is the People of SW</p>
+        {dataKeys.map((p) => {
+          return (
+            <ul key={p}>
+              <li>{p}</li>
+            </ul>
+          )
+        })}
+      </>
+    )
+  }
+  if (!people) {
+    return (
+      <>
+        <p>This is the People of SW</p>
+        <p>Loading data...</p>
+      </>
+    )
+  }
 }
