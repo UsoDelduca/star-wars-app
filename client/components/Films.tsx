736bf34c117ebd9488c3e39afd9d98606921b8dc
Rodolfo Delduca <usodelduca@gmail.com>
2023-11-22 17:21:39 +1300

    add films page

--- client/components/Films.tsx
+++ client/components/Films.tsx
@@ -1,3 +1,85 @@
+import { Key, useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import request from 'superagent'
+import LoadingSpinner from '../UI/UX/LoadingSpinner'
+
+const swURL = 'https://swapi.dev/api/films/'
+
 export function Films() {
-  return <p className="bg-white pl-1 ">This are the Films of SW</p>
+  const [films, setFilms] = useState<any>()
+
+  async function getPeople(swURL: string) {
+    const res = await request.get(swURL)
+    setFilms(res.body)
+  }
+  useEffect(() => {
+    getPeople(swURL)
+  }, [])
+
+  if (films) {
+    const filmsResult = films.results
+    console.log('FILMS: ', filmsResult)
+
+    return (
+      <>
+        <div className="bg-gradient-to-r from-sky-500 to-indigo-500">
+          <p className="bg-white pl-1 ">This are the Films of SW</p>
+
+          {filmsResult.map(
+            (
+              p: {
+                title: Key | string | null | undefined
+                url: string
+
+                next: string
+              },
+              idx: number
+            ) => {
+              const filmsId = p.url.slice(28).split('/')[0]
+              {
+                console.log('ID: ', filmsId)
+              }
+              return (
+                <ul key={idx} className="pl-1">
+                  <li>
+                    <Link to={filmsId}>{p.title}</Link>
+                  </li>
+                </ul>
+              )
+            }
+          )}
+
+          <div>
+            {films.previous && (
+              <button
+                onClick={() => getPeople(films.previous)}
+                className="bg-white mr-1 text-blue-600 hover:text-blue-900 font-semibold py-2 px-4 rounded"
+              >
+                Previous
+              </button>
+            )}
+            {films.next && (
+              <button
+                onClick={() => getPeople(films.next)}
+                className="bg-white text-blue-600 hover:text-blue-900 font-semibold py-2 px-4 rounded"
+              >
+                Next
+              </button>
+              //add number of pages based on the amount of content //
+            )}
+          </div>
+        </div>
+      </>
+    )
+  }
+  if (!films) {
+    return (
+      <>
+        <p className="bg-white pl-1 ">This are the Films of SW</p>
+        <div className="m-2">
+          <LoadingSpinner />
+        </div>
+      </>
+    )
+  }
 }
