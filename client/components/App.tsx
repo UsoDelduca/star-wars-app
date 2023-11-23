0cde0ae7d1418fc09a79286dd82adae526515b94
Rodolfo Delduca <usodelduca@gmail.com>
2023-11-23 18:28:47 +1300

    change background to App component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -4,9 +4,11 @@ import { Header } from './Header'
 function App() {
   return (
     <>
-      <Header />
-      <div className="bg-white opacity-80">
-        <Outlet />
+      <div className="bg-black bg-dotted-white bg-dotted-spacing-2 p-5 font-8bit">
+        <Header />
+        <div className="bg-white opacity-80">
+          <Outlet />
+        </div>
       </div>
     </>
   )
