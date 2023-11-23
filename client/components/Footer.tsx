5efffa39c87a2bf6dd57b37ee338631cf4ef567a
Rodolfo Delduca <usodelduca@gmail.com>
2023-11-23 18:45:51 +1300

    add footer component

---
+++ client/components/Footer.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+  return (
+    <div className="bg-white opacity-80 pl-1 pr-1 flex justify-end">
+      ©Rodolfo Delduca - 2023
+      <a href="https://github.com/UsoDelduca" className="pr-1 pl-1">
+        GitHub
+      </a>
+      <a href="https://www.linkedin.com/in/udelduca/">LinkedIn</a>
+    </div>
+  )
+}
