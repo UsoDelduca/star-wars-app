ea08257b87b5c67888825461ce0386f6899a4805
Rodolfo Delduca <usodelduca@gmail.com>
2023-11-23 17:39:45 +1300

    add logic to Header

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,8 +1,54 @@
-export function Header(props: string[]) {
-  console.log(props)
-  return (
-    <h2 className="bg-white pl-1 text-lg p-2">
-      These are the {props[0]} of SW
-    </h2>
-  )
+import { Link } from 'react-router-dom'
+import LoadingSpinner from '../UI/UX/LoadingSpinner'
+import { useEffect, useState } from 'react'
+import request from 'superagent'
+
+export function Header() {
+  const [data, setData] = useState()
+
+  async function getSWData() {
+    const res = await request.get('https://swapi.dev/api')
+    setData(res.body)
+  }
+
+  useEffect(() => {
+    getSWData()
+  }, [])
+
+  if (data) {
+    const dataKeys = Object.keys(data)
+    return (
+      <div className="flex flex-col">
+        <h1 className="text-2xl w-fit bg-white opacity-80 p-1 rounded">
+          This is SW Wiki!
+        </h1>
+
+        <div className="flex flex-row">
+          {dataKeys.map((category) => {
+            return (
+              <>
+                <div className="flex flex-row">
+                  <li key={category} className="list-none">
+                    <Link
+                      to={category}
+                      className="bg-white opacity-80 mr-0.5 inline-block border-1 border-t border-r rounded-t py-2 px-4 text-blue-600 hover:text-blue-900 font-semibold"
+                    >
+                      {category.toUpperCase()}
+                    </Link>
+                  </li>
+                </div>
+              </>
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
+  if (!data) {
+    return (
+      <>
+        <LoadingSpinner />
+      </>
+    )
+  }
 }
