8f5ea634509aa4c7a79ce308cde7d202f544a712
Rodolfo Delduca <usodelduca@gmail.com>
2023-12-04 18:33:40 +1300

    add logic to vehicle component

--- client/components/Vehicle.tsx
+++ client/components/Vehicle.tsx
@@ -1,8 +1,44 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import request from 'superagent'
+
+const swURL = 'https://swapi.dev/api/vehicles/'
+
 export function Vehicle() {
-  return (
-    <>
-      <p>This is a very important vehicle in SW</p>
-      <p>These people have used this vehicle</p>
-    </>
-  )
+  const [vehicle, setVehicle] = useState<any>()
+  const params = Number(useParams().id)
+
+  async function getVehicle(swURL: string, id: number) {
+    const res = await request.get(`${swURL}${id}`)
+    setVehicle(res.body)
+  }
+  useEffect(() => {
+    getVehicle(swURL, params)
+  }, [])
+
+  console.log(vehicle)
+
+  if (vehicle) {
+    return (
+      <>
+        <h3 className="ml-1">
+          The {vehicle.name} is a very important vehicle in SW
+        </h3>
+        <div className="ml-2 p-2">
+          <p>
+            {vehicle.name} is a {vehicle.vehicle_class} type of vehicle that can
+            carry up to {vehicle.cargo_capacity} kg of cargo
+          </p>
+          <p>
+            The {vehicle.name} can carry {vehicle.passengers} people as
+            passengers, and need {vehicle.crew} crew to be operated
+          </p>
+          <p>
+            It was manufactured by {vehicle.manufacturer}, and it costs{' '}
+            {vehicle.cost_in_credits} credits to purchase one
+          </p>
+        </div>
+      </>
+    )
+  }
 }
