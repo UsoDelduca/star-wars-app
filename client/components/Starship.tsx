7b7568cfc723337e9e1a09fd6fbf0a44287f43ce
Rodolfo Delduca <usodelduca@gmail.com>
2023-12-04 18:33:28 +1300

    add logic to starship component

--- client/components/Starship.tsx
+++ client/components/Starship.tsx
@@ -1,8 +1,44 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import request from 'superagent'
+
+const swURL = 'https://swapi.dev/api/starships/'
+
 export function Starship() {
-  return (
-    <>
-      <p>This is a very important starship in SW</p>
-      <p>These are the people who have been in one of them</p>
-    </>
-  )
+  const [starship, setStarship] = useState<any>()
+  const params = Number(useParams().id)
+
+  async function getStarship(swURL: string, id: number) {
+    const res = await request.get(`${swURL}${id}`)
+    setStarship(res.body)
+  }
+  useEffect(() => {
+    getStarship(swURL, params)
+  }, [])
+
+  console.log(starship)
+
+  if (starship) {
+    return (
+      <>
+        <h3 className="ml-1">
+          The {starship.name} is a very important starship in SW
+        </h3>
+        <div className="ml-2 p-2">
+          <p>
+            The {starship.name} is a {starship.starship_class} type of starship
+            that can carry up to {starship.cargo_capacity} kg of cargo
+          </p>
+          <p>
+            The {starship.name} can carry {starship.passengers} people as
+            passengers, and need {starship.crew} crew to be operated
+          </p>
+          <p>
+            It was manufactured by {starship.manufacturer}, and it costs{' '}
+            {starship.cost_in_credits} credits to purchase one
+          </p>
+        </div>
+      </>
+    )
+  }
 }
